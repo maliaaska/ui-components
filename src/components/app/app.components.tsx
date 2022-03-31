900325429142383c990effd3037c9df06d58b72d
Piotr Pliszczyński <piotr.pilczynski@pl.abb.com>
2022-03-31 12:14:34 +0200

    Refactor App Component and index

---
+++ src/components/app/app.components.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import { HomePage } from '../../pages/home/home.page'
+
+export const App = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+    </Routes>
+  )
+}
