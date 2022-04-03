d96ba74ff47beda8ef9a5ee469adfad4c70714a7
Piotr Pliszczyński <piotr.pilczynski@hitachi-powergrids.com>
2022-04-03 17:03:49 +0200

    Refactor contacts controller Add Prettier, ESLint, nodemon for better dev experience

--- mock-api/index.mjs
+++ mock-api/index.mjs
@@ -1,24 +1,24 @@
 import express from 'express'
 import bodyParser from 'body-parser'
 import cors from 'cors'
-import ContactsController from "./controllers/contacts/contacts.controller.js";
+import ContactsController from './controllers/contacts/contacts.controller.js'
 
 const app = express()
 const port = 3500
 
 app.use((req, res, next) => {
-    bodyParser.json()(req, res, err => {
-        if (err) {
-            console.warn('Bad request => 400')
-            return res.sendStatus(400); // Bad request
-        }
+  bodyParser.json()(req, res, (err) => {
+    if (err) {
+      res.sendStatus(400)
+    }
 
-        next();
-    });
-});
+    next()
+  })
+})
 app.use(cors())
 app.use('/contacts', ContactsController)
 
 app.listen(port, () => {
-    console.log(`Mock API is listening on port ${port}`)
+  // eslint-disable-next-line no-console
+  console.log(`Mock API is listening on port ${port}`)
 })
