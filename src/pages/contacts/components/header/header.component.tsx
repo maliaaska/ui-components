9e708de6ccedd25e0d32f8c790f49ea868ba894c
Arkadiusz Milewski <asmilewski@gmail.com>
2022-04-04 18:19:57 +0200

    font awesome added and working

--- src/pages/contacts/components/header/header.component.tsx
+++ src/pages/contacts/components/header/header.component.tsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import { HeaderStyled, HeaderLeftBox, HeaderRightBox } from './header.style'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faUserPlus } from '@fortawesome/free-solid-svg-icons';
+
 
 interface IHeaderProps {
   onAddNewContact?: () => void
@@ -12,7 +15,7 @@ export const Header: React.VFC<IHeaderProps> = ({ onAddNewContact }) => {
         <h1>Contacts</h1>
       </HeaderLeftBox>
       <HeaderRightBox>
-        <div>
+        <div><FontAwesomeIcon icon={faUserPlus} />
           <button onClick={onAddNewContact}>Add new Contact</button>
         </div>
       </HeaderRightBox>
